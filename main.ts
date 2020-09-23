f62db15ce94f2ef54f86d1998a0b8c1360c10e06
hfecheverri5 <71779797+hfecheverri5@users.noreply.github.com>
2020-09-23 10:49:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
